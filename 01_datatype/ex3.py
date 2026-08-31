592c5f0a55f8fd05e0ba0f38829522b3e986af1e
hwangyuchan42-droid <hwangyuchan42@gmail.com>
2026-08-31 11:26:17 +0900

    O0oooOoOOOO0o

---
+++ 01_datatype/ex3.py
@@ -0,0 +1,29 @@
+#불리언(bool)
+#true, false
+
+a = True
+print(a, type(a))
+
+print(2<3)
+print(2>3)
+print(2==3)
+print(2!=3)
+
+print("apple" == "banana")
+
+#bool()
+
+print(bool(3))
+print(bool(0))
+print(bool("hello"))
+print(bool(""))
+print(bool([10]))
+print(bool([]))
+
+#a NOne 자료형
+a = None
+print(a, type(a))
+print(bool(a))
+
+if a is None:
+    print("a is None")
